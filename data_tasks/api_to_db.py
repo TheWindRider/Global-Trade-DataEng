44d0484f72f9cc1c0c2b962d0062de8ffa27e845
Chenguang Yang <chenguang.yang@bigfishgames.com>
2022-10-06 09:27:21 -0700

    Expand to new data source and new database for text data

--- data_tasks/api_to_db.py
+++ data_tasks/api_to_db.py
@@ -3,6 +3,7 @@ import json
 import pymongo
 import requests
 from typing import List
+from elasticsearch import Elasticsearch, helpers
 
 class TaskApiMongoDB:
     """
@@ -56,3 +57,48 @@ class TaskApiMongoDB:
             ]
         )
         print(result.bulk_api_result)
+
+class TaskApiElasticSearch:
+    """
+    https://newsapi.org
+    """
+    def __init__(self, api_config, db_config):
+        self.api_base_url = api_config['API_URL']
+        self.api_token = api_config['API_TOKEN']
+        self.db_host = db_config["HOST"]
+        self.db_port = db_config["PORT"]
+        self.db_user = db_config["ACCESS_KEY"]
+        self.db_password = db_config["ACCESS_SECRET"]
+        self.db_client = Elasticsearch([{
+            'host': self.db_host,
+            'port': self.db_port,
+            'use_ssl': True,
+            'http_auth': (self.db_user, self.db_password)
+        }])
+
+    def news_api_to_es(self, date_range: [str, str], 
+        query: str = '"international trade" OR "global trade"'
+    ):
+        date_begin, date_end = date_range
+
+        request_url = (
+            f'{self.api_base_url}'
+            f'?apiKey={self.api_token}'
+            f'&q={query}&from={date_begin}&to={date_end}'
+            f'&sortBy=relevancy'
+        )
+        response = requests.get(request_url)
+        if response.status_code != 200:
+            return {"error_msg": f"not able to request data from {request_url}"}
+
+        if not self.db_client.indices.exists(index='news_global_trade'):
+            self.db_client.indices.create(index='news_global_trade')
+
+        result = helpers.bulk(
+            self.db_client, 
+            [
+                {'_index': 'news_global_trade', '_source': news}
+                for news in response.json()["articles"]
+            ]
+        )
+        print(result)
